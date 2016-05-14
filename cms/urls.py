89e327de5d42bc72d776c48a1e3d42d8d39fd8ed
porimol <p.c_roy@yahoo.com>
2016-05-15 01:33:17 +0600

    remove frontend app

--- cms/urls.py
+++ cms/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home, name='home'),
+    url(r'^(?P<post_id>[0-9]+)/single/$', views.signle_post, name='signle_post'),
 ]
--- cms/views.py
+++ cms/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.template import loader
+from cms.models import Post
 
 # Index view
 def index(request):
     return HttpResponse("Hi")
+
+# home view
+def home(request):
+    blog_posts = Post.objects.order_by('-pub_date')[:10]
+    template = loader.get_template('frontend/home.html')
+    context = {
+        'blog_posts': blog_posts,
+    }
+    return HttpResponse(template.render(context, request))
+
+# Get the single post details
+def signle_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    template = loader.get_template('frontend/single.html')
+    context = {
+        'post': post,
+    }
+    return HttpResponse(template.render(context, request))
