1864384ebc3e3e267a3ba885dc322f5994edffa7
porimol <p.c_roy@yahoo.com>
2016-05-20 01:33:54 +0600

    admin model class add

--- cms/admin.py
+++ cms/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from .models import Category, Tag, Post
 
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = [ "name", "slug", "description"]
+    class Meta:
+        model = Category
+
 # Register your Category
-admin.site.register(Category)
-admin.site.register(Tag)
-admin.site.register(Post)
+admin.site.register(Category, CategoryModelAdmin)
+
+class TagModelAdmin(admin.ModelAdmin):
+    list_display = [ "name", "slug", "description"]
+    class Meta:
+        model = Tag
+
+# Register your Tag
+admin.site.register(Tag, TagModelAdmin)
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = [ "title", "description", "pub_date"]
+    class Meta:
+        model = Post
+
+# Register your Post
+admin.site.register(Post, PostModelAdmin)
