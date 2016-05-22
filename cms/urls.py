2170b915245dea955d95dfaade76318dce0b42d8
porimol <p.c_roy@yahoo.com>
2016-05-22 13:29:46 +0600

    categories/(id)/posts add

--- cms/urls.py
+++ cms/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^(?P<post_id>[0-9]+)/single/$', views.single_post, name='single_post'),
-    url(r'^(?P<category_id>[0-9]+)/$', views.post_by_category, name='post_by_category'),
+    url(r'^(?P<category_id>[0-9]+)/posts/$', views.post_by_category, name='post_by_category'),
 ]
