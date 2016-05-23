b967d5be33b2f2f07d68aa4fe77fa8f221cdde93
porimol <p.c_roy@yahoo.com>
2016-05-24 03:32:55 +0600

    image path and statics(js,css) path issues fix

--- blog/settings.py
+++ blog/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'django.template.context_processors.static',
             ],
         },
     },
@@ -124,11 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR + '/static/'
-
 PROJECT_ROOT = os.path.join(os.path.dirname(__file__), '..')
 SITE_ROOT = PROJECT_ROOT
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
 
 MEDIA_URL = '/media/featured_photos/'
 MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
